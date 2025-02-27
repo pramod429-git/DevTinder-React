1a378f6ea52ef3ae1a183e83f41d573d50621989
pramod429-git <18kppramod@gmail.com>
2025-02-27 21:58:59 +0530

    base url changes

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,5 @@
-//export const BASE_URL = "http://localhost:7777";// only works in local
-export const BASE_URL = "/api";
+//export const BASE_URL = "http://localhost:7777"; // only works in local
+//export const BASE_URL = "/api";
+
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
